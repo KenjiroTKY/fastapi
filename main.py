594bf6b13761fe0b0c926dbef3533aa6dbcb77f1
KenjiroTky <KenjiroTky@gmail.com>
2022-10-08 19:14:07 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from routers import route_todo, route_auth
+from schemas import SuccessMsg, CsrfSettings
+from fastapi_csrf_protect import CsrfProtect
+from fastapi_csrf_protect.exceptions import CsrfProtectError
+
+app = FastAPI()
+app.include_router(route_todo.router)
+app.include_router(route_auth.router)
+
+@CsrfProtect.load_config
+def get_csrf_config():
+    return CsrfSettings()
+
+@app.exception_handler(CsrfProtectError)
+def csrf_protect_exception_handler(request: Request, exc: CsrfProtectError):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={ 'detail':  exc.message
+        }
+    )
+
+@app.get("/", response_model=SuccessMsg)
+def root():
+    return {"message": "Welcome to FastAPI"}
